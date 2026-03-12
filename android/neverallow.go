c4955a10e2a87192c0d59bd883d9a9ab13b13abe
Nathan <nathannxx@proton.me>
2026-03-12 15:49:39 +0100

    Revert "Add vendor_overlay support to fsgen"
    This reverts commit 1bdb6d97270f630541799d6204a9464aee84985d.

--- android/neverallow.go
+++ android/neverallow.go
@@ -360,7 +360,6 @@ func createPrebuiltEtcBpDefineRule() Rule {
 			"prebuilt_first_stage_ramdisk",
 			"prebuilt_radio",
 			"prebuilt_gpu",
-			"prebuilt_vendor_overlay",
 		).
 		DefinedInBpFile().
 		Because("module type not allowed to be defined in bp file")
